54d4b7839d599c44110083632773ef3574b53463
MrXiaoM <coolxiaom95@gmail.com>
2025-08-11 02:15:30 +0800

    Update CommandManager.java

--- src/main/java/dev/mrshawn/deathmessages/command/CommandManager.java
+++ src/main/java/dev/mrshawn/deathmessages/command/CommandManager.java
@@ -30,24 +30,24 @@ public class CommandManager implements CommandExecutor, TabCompleter {
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String cmdLabel, @NotNull String[] args) {
         if ((sender instanceof Player) && !sender.hasPermission(Permission.DEATHMESSAGES_COMMAND.getValue())) {
             sender.sendMessage(Messages.formatMessage("Commands.DeathMessages.No-Permission"));
-            return false;
+            return true;
         } else if (args.length == 0) {
             for (String s : Messages.formatMessage(Messages.getInstance().getConfig().getStringList("Commands.DeathMessages.Help"))) {
                 sender.sendMessage(s);
             }
-            return false;
+            return true;
         } else {
             ICommand cmd = get(args[0]);
             if (cmd != null) {
                 ArrayList<String> a = new ArrayList<>(Arrays.asList(args));
                 a.remove(0);
                 cmd.onCommand(sender, a.toArray(new String[0]));
-                return false;
+                return true;
             }
             for (String s2 : Messages.formatMessage(Messages.getInstance().getConfig().getStringList("Commands.DeathMessages.Help"))) {
                 sender.sendMessage(s2);
             }
-            return false;
+            return true;
         }
     }
 
